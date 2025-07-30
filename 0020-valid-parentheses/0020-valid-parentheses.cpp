f0227696c499a0e14f2aad2c459afb050f69c717
VIDHAN GUPTA <148654884+Crusherbolt@users.noreply.github.com>
2025-07-31 00:22:48 +0530

    Time: 0 ms (100%), Space: 8.8 MB (33.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char ch : s) {
+            if(!st.empty()) {
+                if(ch == ')' && st.top() == '(') {
+                    st.pop();
+                }
+                else if(ch == ']' && st.top() == '[') {
+                    st.pop();
+                }
+                else if(ch == '}' && st.top() == '{') {
+                    st.pop();
+                }
+                else {
+                    st.push(ch);
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+        return st.size() == 0;
+    }
+};
